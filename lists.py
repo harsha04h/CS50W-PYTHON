da82354d3f6b2f771ce9230dd204a1114e4fb169
harsha04h <harsha004h@gmail.com>
2025-12-29 20:58:16 +0530

    add,sort elements in list

---
+++ lists.py
@@ -0,0 +1,14 @@
+# define list of names
+names=["Harry","Hermione","Ron","Ginny"]
+
+print(names)
+print(names[1])
+ 
+names.append("Draco")
+names.sort()
+print(names)
+
+numbers=[10,12,1,5,23,11]
+print(numbers)
+numbers.sort()
+print(f"sorted number:{numbers}")
