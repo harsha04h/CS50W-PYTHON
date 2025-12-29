98886a5d112e23e8fede77751dff8841ef4bf026
harsha04h <harsha004h@gmail.com>
2025-12-29 21:46:47 +0530

    list,tuple,set,dictionary

---
+++ sequences.py
@@ -0,0 +1,21 @@
+name="Harsha"
+print(name[0])
+print(name[1])
+
+n=["harry","hermione","ron"]  #list of names
+print(n)
+print(n[0])
+
+coordinate =(10.0,20.0) #tuple
+print(coordinate)
+
+s=set() #empty set
+#add elements to set
+s.add(1)
+s.add(2)  
+s.add(3)
+s.add(4)
+print(s)
+s.remove(2)
+print(f"after removing:{s}")
+print(f"The set has {len(s)} elements")
